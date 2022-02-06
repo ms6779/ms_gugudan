fd7c2bec6fcc85d42813368b31a79d3bc3a33608
yunminsun <34468227+yunminsun@users.noreply.github.com>
2022-02-06 19:59:02 +0900

    first commit

---
+++ gugudan.py
@@ -0,0 +1,11 @@
+result = ""
+for i in range(1,9):
+    for j in range(9):
+        result += str(i+1) + ' * ' + str(j+1) + ' = ' + str((i+1) * (j+1)) + ' '
+    else:
+        if i != 8:
+            result += '\n'
+        else:
+            result += ''
+
+print(result)
