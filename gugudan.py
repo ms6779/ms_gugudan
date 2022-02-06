f976c80a4802ab09631d6adebd352d2fda2bd649
yunminsun <34468227+yunminsun@users.noreply.github.com>
2022-02-06 20:24:28 +0900

    세로 구구단

--- gugudan.py
+++ gugudan.py
@@ -1,9 +1,10 @@
 result = ""
-for i in range(1,9):
-    for j in range(9):
-        result += str(i+1) + ' * ' + str(j+1) + ' = ' + str((i+1) * (j+1)) + ' '
+
+for i in range(9):
+    for j in range(1,9):
+        result += str(j+1) + ' * ' + str(i+1) + ' = ' + (str((j+1) * (i+1)) if (j+1) * (i+1) > 9 else ' ' + str((j+1) * (i+1))) + '\t'
     else:
         if i != 8:
             result += '\n'
-## MS
+
 print(result)
